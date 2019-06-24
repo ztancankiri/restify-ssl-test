26aee961b268d69687f95d028f2f1377f64e07be
Cankiri <zafer-tan.cankiri@iao.fraunhofer.de>
2019-06-24 08:53:43 +0200

    Security bug is fixed.

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const fs = require('fs');
-const restify = require('restify');
+const fs = require("fs");
+const restify = require("restify");
+const passphrase = require("./ssl/passphrase");
 
 const port = 3443;
 const httpsOptions = {
-    key: fs.readFileSync('./ssl/key.pem'),
-    cert: fs.readFileSync('./ssl/cert.pem'),
-    passphrase: '120196'
+  key: fs.readFileSync("./ssl/key.pem"),
+  cert: fs.readFileSync("./ssl/cert.pem"),
+  passphrase: passphrase.passphrase
 };
 
 const server = restify.createServer(httpsOptions);
 
-server.get('/hello', (req, res) => {
-    res.send('Hello World!');
+server.get("/hello", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(port, console.log('Listening on port ' + port + '...'));
+server.listen(port, console.log("Listening on port " + port + "..."));
